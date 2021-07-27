9d1cc6b04d580213a686f7f48d6291b6285d4500
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-07-26 23:58:53 -0300

    add new background and image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './development2.svg';
 import './App.css';
 
 function App() {
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1 className="title">
           Página em construção!
-        </p>
+        </h1>
       </header>
     </div>
   );
