5dbaf68d309373671a07063c346ca05cdccc91a0
Rodrigo <rodrigoalmeidagit@gmail.com>
2021-07-28 22:31:14 -0300

    fixed semantica citação html

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3><a href="https://storyset.com/online" className="attribution">Illustrations by Storyset</a></h3>
+        <cite><a href="https://storyset.com/online" className="attribution">Illustrations by Storyset</a></cite>
         <h1 className="title">
           Página em construção!
         </h1>
